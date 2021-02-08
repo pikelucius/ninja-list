04719a7740ceae6fd40d444a8ad1c24419861066
Peter Fagerkvist <p.fagerkvist@gmail.com>
2021-02-08 17:23:41 +0100

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+           Copyright Ninja List 2021
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                 <title>Ninja List | About </title>
+                <meta name="keywords" content="ninjas"/>        
+            </Head>
+            <div>
+                <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rem ipsam minima odit exercitationem eaque amet beatae veritatis similique accusantium?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rem ipsam minima odit exercitationem eaque amet beatae veritatis similique accusantium?</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
